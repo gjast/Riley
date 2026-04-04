1c7bfbed194d87ecbb843a2032cb09913dd93765
karim <gtder41@gmail.com>
2026-04-04 22:26:06 +0500

    ор

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -8,7 +8,7 @@ export default function Case({ img, title, to }) {
         alt=""
         draggable={false}
         onDragStart={(e) => e.preventDefault()}
-        className="aspect-4/3 w-full select-none rounded-xl border border-[#252526] object-cover sm:rounded-2xl md:rounded-[16px]"
+        className="w-full h-full aspect-square select-none rounded-xl border border-[#252526] object-cover sm:rounded-2xl md:rounded-[16px]"
       />
       <p className="text-[15px] font-medium leading-snug sm:text-[16px] md:text-[18px]">
         {title}
