845ea654368905102b91428a6aabf79d30b8c0bd
karim <gtder41@gmail.com>
2026-04-05 03:27:02 +0500

    павпвп

--- src/pages/Portfolio/blocks/Cart.jsx
+++ src/pages/Portfolio/blocks/Cart.jsx
@@ -29,29 +29,6 @@ export default function Cart({ img, title, description, href }) {
         <p className="text-[16px] leading-[150%] tracking-[-0.02em] mt-[14px] mb-[24px] text-[#8B8B8B] text-balance">
           {description}
         </p>
-        <a
-          href={href}
-          className="text-[16px] leading-[150%] font-medium tracking-[-0.02em] text-white flex items-center gap-[4px] cursor-pointer group"
-        >
-          {t("portfolio.viewLink")}{" "}
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="transition-transform duration-200 ease-in-out group-hover:translate-x-[5px]"
-          >
-            <path
-              d="M6.68359 14.9401L11.5736 10.0501C12.1511 9.47256 12.1511 8.52756 11.5736 7.95006L6.68359 3.06006"
-              stroke="white"
-              strokeWidth="1.5"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </a>
         </div>
       </div>
   );
